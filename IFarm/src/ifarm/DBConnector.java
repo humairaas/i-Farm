db64b16b7e8ede07c71848449b38d7a6939c2746
Humaira' AS <you@example.com>
2022-05-09 23:49:27 +0800

    Connect to DB

--- IFarm/src/ifarm/DBConnector.java
+++ IFarm/src/ifarm/DBConnector.java
@@ -8,6 +8,7 @@ package ifarm;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.sql.*;
+import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -15,33 +16,57 @@ public class DBConnector {
     private final String DB_URL = "jdbc:mysql://localhost:3306/i_farm?zeroDateTimeBehavior=convertToNull";
     private final String USERNAME = "i_farm_user";
     private final String PASSWORD = "ifarm123";
+    
+    java.sql.Connection conn = null;
+    java.sql.Statement stmt = null;
 
     public void start() {
-        java.sql.Connection conn = null;
-        java.sql.Statement stmt = null;
+        try{
+            conn = DriverManager.getConnection(DB_URL, USERNAME, PASSWORD);
+            System.out.println("Connected to database.");
+            
+        }catch(SQLException e){
+            System.out.println("Not connected to database.");
+            System.err.println(e);
+        }
+    }
+    
+    public void insert() {
+        try{
+            conn = DriverManager.getConnection(DB_URL, USERNAME, PASSWORD);
+            System.out.println("Connected to database.");
+            
+            stmt = conn.createStatement();
+            try {
+                File myObj = new File("C:\\Users\\User\\Documents\\NetBeansProjects\\i-Farm\\IFarm\\src\\ifarm\\txtFiles\\Fertilizers.txt");
+                try (Scanner myReader = new Scanner(myObj)) {
+                    while (myReader.hasNextLine()) {
+                        String data = myReader.nextLine();
+                        String sql = "INSERT INTO `fertilizers` (`fertilizer_id`, `name`, `unit_type`, `timestamp_create`, `timestamp_update`) VALUES (NULL, '"+data+"', 'pack', current_timestamp(), NULL);";
+                        stmt.executeUpdate(sql);
+                        System.out.println("Inserted "+data+" records into the table");
+                    }
+                }
+              } catch (FileNotFoundException e) {
+                System.out.println("An error occurred.");
+              }
+            
+        }catch(SQLException e){
+            System.out.println("Not connected to database.");
+            System.err.println(e);
+        }
+    }
+    
+    public void randomize() {
+        ArrayList<Integer> numbers = new ArrayList<Integer>();
         
         try{
             conn = DriverManager.getConnection(DB_URL, USERNAME, PASSWORD);
             System.out.println("Connected to database.");
             
             stmt = conn.createStatement();
-//            try {
-//                File myObj = new File("C:\\Users\\User\\Documents\\NetBeansProjects\\i-Farm\\IFarm\\src\\ifarm\\txtFiles\\Fertilizers.txt");
-//                Scanner myReader = new Scanner(myObj);
-//                while (myReader.hasNextLine()) {
-//                  String data = myReader.nextLine();
-//                  String sql = "INSERT INTO `fertilizers` (`fertilizer_id`, `name`, `unit_type`, `timestamp_create`, `timestamp_update`) VALUES (NULL, '"+data+"', 'pack', current_timestamp(), NULL);";
-//                  stmt.executeUpdate(sql);
-//                  System.out.println("Inserted "+data+" records into the table");
-//                }
-//                myReader.close();
-//              } catch (FileNotFoundException e) {
-//                System.out.println("An error occurred.");
-//                e.printStackTrace();
-//              }
-//        Random rand = new Random();
-//        int rand_int1 = rand.nextInt(100)+1;
-            
+            Random rand = new Random();
+            int rand_int1 = rand.nextInt(100)+1;
 
             String sql = "";
             stmt.executeUpdate(sql);
