2560f8dc6c137efc8d278611f9c3c69610e8c24f
Songyuxuan-um <65456072+Songyuxuan-um@users.noreply.github.com>
2022-05-20 14:06:09 +0800

    Update VisualizeActivities.java

--- IFarm/src/ifarm/VisualizeActivities.java
+++ IFarm/src/ifarm/VisualizeActivities.java
@@ -12,9 +12,7 @@ import java.sql.*;
 public class VisualizeActivities {
     static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
 	static final String DB_URL = "jdbc:mysql://localhost:3306/i_farm_user?useUnicode=true&characterEncoding=UTF-8&zeroDateTimeBehavior=convertToNull&useSSL=true";
-	// 数据库的用户名与密码
-	//用户名：root  密码：123456
-	//请根据实际数据库的信息进行修改
+	
 	static final String USER = "root";
     static final String PASS = "";
 	public static void main(String[] args) {
@@ -27,12 +25,12 @@ public class VisualizeActivities {
 //            conn = DriverManager.getConnection(DB_URL,USER,PASS);
 //            stmt = conn.createStatement();
 //            String sql;
-//            sql = "SELECT * FROM activity";//搜索login表
+//            sql = "SELECT * FROM activity";
 //            ResultSet rs = stmt.executeQuery(sql);
 //            while(rs.next()){
-//                String action  = rs.getString("action");//得到“id”列的值
-//                String unit = rs.getString("unit");//得到“password”列的值
-//                int quantity = rs.getInt("quantity");//得到所有值
+//                String action  = rs.getString("action");
+//                String unit = rs.getString("unit");
+//                int quantity = rs.getInt("quantity");
 //                
 //                
 //                
@@ -60,24 +58,22 @@ public class VisualizeActivities {
 //            }
 //        }
 //	    System.out.print("搜索完毕\n");
-//	    //开始修改数据
+//	    
         try{
-            // 注册 JDBC 驱动
+            
             Class.forName(JDBC_DRIVER);
-            // 打开链接
-            //连接数据库
+            
             conn = DriverManager.getConnection(DB_URL,USER,PASS);
-            // 执行查询
-            //实例化Statement对象
+            
             stmt = conn.createStatement();
             String sql;
             
 //            sql ="SELECT * FROM activity where unit='g'";
 //             ResultSet ts = stmt.executeQuery(sql);
 //            while(ts.next()){
-//                String action  = ts.getString("action");//得到“id”列的值
-//                String unit = ts.getString("unit");//得到“password”列的值
-//                int quantity = ts.getInt("quantity");//得到所有值
+//                String action  = ts.getString("action");
+//                String unit = ts.getString("unit");
+//                int quantity = ts.getInt("quantity");
 //                int quan = quantity*2;
 //                
 //                
@@ -88,12 +84,12 @@ public class VisualizeActivities {
 //                System.out.print("\n");
 //            }
 //            ts.close();
-            sql = "SELECT * FROM activity ";//搜索login表
+            sql = "SELECT * FROM activity ";
             ResultSet rs = stmt.executeQuery(sql);
             
    while(rs.next()){ 
        
-                Double quantity = rs.getDouble("quantity");//得到所有值
+                Double quantity = rs.getDouble("quantity");
                
                Double quan = quantity/1000;
                Double quan1 = quantity*500/1000;
@@ -104,61 +100,57 @@ public class VisualizeActivities {
                 
                sql="UPDATE activity SET quantity=? WHERE unit = 'g'and id = ?"; 
             
-            ps=conn.prepareStatement(sql);//修改数据预处理
+            ps=conn.prepareStatement(sql);
           ps.setDouble(1,quan);
           int id = rs.getInt("id");
           ps.setInt(2, id);
           
             
 //          
-        	ps.executeUpdate();//执行修改数据
+        	ps.executeUpdate();
             
              sql="UPDATE activity SET unit='kg' WHERE unit='g'and id = ?"; 
             
-            ps=conn.prepareStatement(sql);//修改数据预处理
+            ps=conn.prepareStatement(sql);
             ps.setInt(1, id);
-        	ps.executeUpdate();//执行修改数据
+        	ps.executeUpdate();
 //            
 ////            
             sql="UPDATE activity SET quantity=? WHERE unit='ml'and id = ?"; 
-            //向login表里修改数据^
-            //注：几个问号几个ps.setString，上面的语句中有两个?,所以下面有两个ps.setString
-            ps=conn.prepareStatement(sql);//修改数据预处理
+            
+            ps=conn.prepareStatement(sql);
             
             ps.setDouble(1,quan);
             
           ps.setInt(2, id);
         	ps.executeUpdate();
-//执行修改数据
-            // 完成后关闭
+
             
                   sql="UPDATE activity SET unit='l' WHERE unit='ml'and id = ?"; 
             
-            ps=conn.prepareStatement(sql);//修改数据预处理
+            ps=conn.prepareStatement(sql);
             ps.setInt(1, id);
-        	ps.executeUpdate();//执行修改数据
-            // 完成后关闭      
+        	ps.executeUpdate();
+            
             
             
             
             sql="UPDATE activity SET quantity=? WHERE unit='pack(500g)'and id = ?"; 
-            //向login表里修改数据^
-            //注：几个问号几个ps.setString，上面的语句中有两个?,所以下面有两个ps.setString
-            ps=conn.prepareStatement(sql);//修改数据预处理
+            
+            ps=conn.prepareStatement(sql);
             
             ps.setDouble(1,quan1);
             
           ps.setInt(2, id);
         	ps.executeUpdate();
-//执行修改数据
-            // 完成后关闭
+
             
                   sql="UPDATE activity SET unit='kg' WHERE unit='pack(500g)'and id = ?"; 
             
-            ps=conn.prepareStatement(sql);//修改数据预处理
+            ps=conn.prepareStatement(sql);
             ps.setInt(1, id);
-        	ps.executeUpdate();//执行修改数据
-            // 完成后关闭   
+        	ps.executeUpdate();
+         
             
             
             
@@ -186,25 +178,25 @@ public class VisualizeActivities {
             stmt.close();
             conn.close();
         }catch(SQLException se){
-            // 处理 JDBC 错误
+            
             se.printStackTrace();
         }catch(Exception e){
-            // 处理 Class.forName 错误
+        
             e.printStackTrace();
         }finally{
-            // 关闭资源
+           
             try{
                 if(stmt!=null) stmt.close();
             }catch(SQLException se2){
-            }// 什么都不做
+            }
             try{
                 if(conn!=null) conn.close();
             }catch(SQLException se){
                 se.printStackTrace();
             }
         }
-        System.out.println("数据修改成功");
-        //修改数据结束
+        System.out.println("Sucess!");
+     
         
 }
 }
