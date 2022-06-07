de835f477125081e800fd239838deaaeafd1727d
husna <nd.hsnaa@gmail.com>
2022-06-08 04:59:11 +0800

    data handler still in progress

--- IFarm/src/ifarm/DataEntryHandler.java
+++ IFarm/src/ifarm/DataEntryHandler.java
@@ -5,10 +5,41 @@
  */
 package ifarm;
 
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+import java.util.concurrent.Future;
+
 /**
  *
  * @author User
  */
-public class DataEntryHandler {
+public class DataEntryHandler implements Runnable {
+    
+    private List<String[]> activities;
+    private Activity activity_class;
+
+    public DataEntryHandler(List<String[]> activities) {
+        this.activities = activities;
+    }
+    
+    @Override
+    public void run() {
+        
+        for (String[] s : activities){
+            
+            String joined = String.join("", s);
+            
+            
+//            activity_class.toTxt(joined);
+            System.out.println(Thread.currentThread().getName() + ": " + joined );
+//            activity_class.toDB(s[8], data[2], quantity[1], Integer.parseInt(quantity[0]), randRow, randField, Integer.parseInt(data[1]), Integer.parseInt(data[0]));
+            
+        }
+
+        
+
+        System.out.println(Thread.currentThread().getName()+"Next entry");
+    }
     
 }
